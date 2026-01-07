d083c01053fcf138c76a6b18eb8bac93ded852bc
Alan Villarreal <nydhoggr@protonmail.com>
2026-01-07 14:58:54 -0600

    feat: Getters and setters created for Task class

--- com/tasktracker/model/Task.java
+++ com/tasktracker/model/Task.java
@@ -17,4 +17,36 @@ public class Task {
         this.description = description;
         createdAt = LocalDateTime.now();
     }
+
+    public int getId() {
+         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void updateTimeStamp() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setStatus(Status statusToUpdate) {
+        this.status = statusToUpdate;
+    }
+
+    public void setDescription(String newDescription) {
+        this.description = newDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
 }
