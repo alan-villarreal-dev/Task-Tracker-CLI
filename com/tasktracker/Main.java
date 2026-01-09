23f0ebaceb2a9bab110e15121c972b1bbac94750
Alan Villarreal <nydhoggr@protonmail.com>
2026-01-09 15:46:56 -0600

    fix: class name fix

--- com/tasktracker/Main.java
+++ com/tasktracker/Main.java
@@ -1,51 +1,56 @@
 import model.Status;
 import service.TaskService;
 
-void main(String[] args) {
+import java.util.Optional;
 
-    // Constraint check: No option selected
-    if (args.length == 0) {
-        IO.println("Incompatible option, exiting...");
-        System.exit(0);
-    }
+public class Main {
+    void main(String[] args) {
 
-
-    String taskToPerform = args[0].toLowerCase();
-
-    TaskService taskService = new TaskService();
-
-    switch (taskToPerform) {
-        case "add" -> taskService.createTask(args[1]);
-
-        case "update" -> taskService.updateTask(Integer.parseInt(args[1]), args[2]);
-
-        case "delete" -> taskService.deleteTask(Integer.parseInt(args[1]));
-
-        case "mark-in-progress" ->  taskService.updateStatus("mark-in-progress", Integer.parseInt(args[1]));
-
-        case "mark-done" ->  taskService.updateStatus("mark-done", Integer.parseInt(args[1]));
-
-        case "list" -> {
-
-            if (args.length == 1) {
-                taskService.showTasks(Optional.empty());
-            }
-
-            if (args[1].equals("done") ) {
-                taskService.showTasks(Optional.of(Status.DONE));
-            }
-
-            if (args[1].equals("todo")) {
-                taskService.showTasks(Optional.of(Status.TODO));
-            }
-
-            if (args[1].equals("in-progress")) {
-                taskService.showTasks(Optional.of(Status.IN_PROGRESS));
-            }
-
+        // Constraint check: No option selected
+        if (args.length == 0) {
+            IO.println("Incompatible option, exiting...");
+            System.exit(0);
         }
 
-        default -> throw new IllegalStateException("Unexpected value: " + taskToPerform);
+
+        String taskToPerform = args[0].toLowerCase();
+
+        TaskService taskService = new TaskService();
+
+        switch (taskToPerform) {
+            case "add" -> taskService.createTask(args[1]);
+
+            case "update" -> taskService.updateTask(Integer.parseInt(args[1]), args[2]);
+
+            case "delete" -> taskService.deleteTask(Integer.parseInt(args[1]));
+
+            case "mark-in-progress" ->  taskService.updateStatus("mark-in-progress", Integer.parseInt(args[1]));
+
+            case "mark-done" ->  taskService.updateStatus("mark-done", Integer.parseInt(args[1]));
+
+            case "list" -> {
+
+                if (args.length == 1) {
+                    taskService.showTasks(Optional.empty());
+                }
+
+                if (args[1].equals("done") ) {
+                    taskService.showTasks(Optional.of(Status.DONE));
+                }
+
+                if (args[1].equals("todo")) {
+                    taskService.showTasks(Optional.of(Status.TODO));
+                }
+
+                if (args[1].equals("in-progress")) {
+                    taskService.showTasks(Optional.of(Status.IN_PROGRESS));
+                }
+
+            }
+
+            default -> throw new IllegalStateException("Unexpected value: " + taskToPerform);
+        }
+
     }
 
 }
